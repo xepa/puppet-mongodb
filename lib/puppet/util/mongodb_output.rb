02b807d8c2eec89227d46dfa1ed4799fe9124cea
Michiel Brandenburg <mbrandenburg@vcn.nl>
2023-03-27 19:41:53 +0200

    Removed some spam see https://github.com/mongodb/mongo/commit/f7f6c6928320873089b443da007de119b926f605

--- lib/puppet/util/mongodb_output.rb
+++ lib/puppet/util/mongodb_output.rb
@@ -3,6 +3,7 @@ module Puppet
     module MongodbOutput
       def self.sanitize(data)
         # Dirty hack to remove JavaScript objects
+        data.gsub!(%r{^({.+"msg":"Started a new thread for the timer service"}\n)}, '') # hack to remove tls / auth spam see https://github.com/mongodb/mongo/commit/f7f6c6928320873089b443da007de119b926f605
         data.gsub!(%r{\w+\((\d+).+?\)}, '\1') # Remove extra parameters from 'Timestamp(1462971623, 1)' Objects
         data.gsub!(%r{\w+\((.+?)\)}, '\1')
 
